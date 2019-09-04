ba24f2712de2c7fad4a7b66a4f87b3aaf987f957
bessistemn1i <eduard.zhulega@yandex.ru>
2019-09-04 15:04:07 +0200

    add smooth scrolling

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import ImageSwitcher from './modules/imageSwitcher';
 import CommentsSlider from './modules/commentsSlider';
 import CountDown from './modules/countdown';
 import PopupOpener from './modules/popupOpener';
+import SmoothScroll from './modules/smoothScroll';
 
 const videoToPlay = new VideoToPlay();
 const tabSwitcher = new TabSwitcher();
@@ -11,3 +12,4 @@ const imageSwitcher = new ImageSwitcher();
 const sliderComments = new CommentsSlider();
 const countdown = new CountDown();
 const popupOpener = new PopupOpener();
+const smoothScroll = new SmoothScroll();
---
+++ src/js/modules/smoothScroll.js
@@ -0,0 +1,63 @@
+class SmoothScroll {
+    constructor() {
+        this.scrollElems = document.querySelectorAll('.main-menu__link');
+        this.addEvents();
+    }
+
+    easeIn(t) {
+        return t;
+    };
+
+    scrollToElem (startTime, currentTime, duration, scrollEndElemTop, startScrollOffset) {
+        const runtime = currentTime - startTime;
+        let progress = runtime / duration;
+        
+        progress = Math.min(progress, 1);
+        
+        const ease = this.easeIn(progress);
+        
+        window.scroll(0, startScrollOffset + (scrollEndElemTop * ease));
+        if(runtime < duration){
+            requestAnimationFrame((timestamp) => {
+            const currentTime = timestamp || new Date().getTime();
+            this.scrollToElem(startTime, currentTime, duration, scrollEndElemTop, startScrollOffset);
+            })
+        }
+    }
+
+    scrolling(scrollElemId) {
+        // Grab all the scroll class anchor elements, use whatever class you like
+        
+        // 2. find that node from the document
+        const scrollEndElem = document.getElementById(scrollElemId);
+            
+        // 3. and well animate to that node.. 
+        const anim = requestAnimationFrame((timestamp) => {
+        const stamp = timestamp || new Date().getTime();
+        const duration = 1200;
+        const start = stamp;
+        
+        const startScrollOffset = window.pageYOffset;
+        const scrollEndElemTop = scrollEndElem.getBoundingClientRect().top;
+        
+        this.scrollToElem(start, stamp, duration, scrollEndElemTop, startScrollOffset);
+        })
+    }
+
+    addEvents() {
+    // Now add an event listeners to those element
+        for(let i = 0; i < this.scrollElems.length; i++){
+        const elem = this.scrollElems[i];
+        
+            elem.addEventListener('click', (e) => {
+            e.preventDefault();
+            
+            // 1. Get the element id to which you want to scroll
+            const scrollElemId = e.target.href.split('#')[1];
+            this.scrolling(scrollElemId);
+        })
+        }
+    }
+}
+
+export default SmoothScroll;
